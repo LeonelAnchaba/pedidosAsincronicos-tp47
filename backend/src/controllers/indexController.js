fe564cf90cc5700ccc23e2ff4ad73ae271bc5cc7
LeonelAnchaba <leonelanchaba@gmail.com>
2024-04-17 15:39:14 -0300

    desde el home relacioné a cada pelicula para que redigija a su formulario especifico

--- backend/src/controllers/indexController.js
+++ backend/src/controllers/indexController.js
@@ -3,6 +3,9 @@ const path = require("path");
 const indexController = {
     home: (req,res) => {
         res.sendFile(path.join(__dirname,"../../../frontend/views/home.html"));
+    },
+    form: (req,res) => {
+        res.sendFile(path.join(__dirname,"../../../frontend/views/formulario.html"))
     }
 }
 
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,6 +5,6 @@ const indexController = require('../controllers/indexController');
 //Rutas
 //Listado de películas
 router.get('/', indexController.home);
-
+router.get('/form/:id', indexController.form);
 
 module.exports = router;
